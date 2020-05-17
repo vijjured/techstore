248e5cc46b5de75ce289c6f40f244a5414cf4c2a
unknown <vbr0856@gmail.com>
2020-05-16 17:45:21 -0700

    MAINT : basic setup for app

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <div>Hello fron tech store</div>;
+}
+
+export default App;
